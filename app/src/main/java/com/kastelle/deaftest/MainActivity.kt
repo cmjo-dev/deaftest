f95bbe216b5a6544af817ad15f6cd1cc365421ab
Camille Quenin <camille.quenin@gmail.com>
2020-04-03 18:54:09 +0200

    [DEV] Add the project skeleton

---
+++ app/src/main/java/com/kastelle/deaftest/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.kastelle.deaftest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/** UI controller for the main interface. */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
